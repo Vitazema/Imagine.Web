b75f00ce6363e06d3f117ed6633952c674a041ce
Vitalii Malozemov <v.malozyomov@itransition.com>
2023-06-29 01:10:52 +0300

    Added header

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -2,14 +2,18 @@ import React, { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
 import Navigation from "../Navigation/Navitgation"
 import classes from "./MainHeader.module.css"
+import logo from "../../assets/logo192.png"
 
 const MainHeader: React.FC = () => {
   const authContext = React.useContext(AuthContext)
 
   return (
     <header className={classes["main-header"]}>
+      <div className="col-5">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
       <h1>Main</h1>
-      <Navigation/>
+      <Navigation />
       {authContext?.isLoggedIn && <p>Logged in as: {authContext.userName}</p>}
       <button onClick={authContext?.login}>Login</button>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import AuthProvider from "./context/AuthContext"
+import React from "react"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )
